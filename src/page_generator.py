67c91e73dbc50c0e07363872ee370df995ae8c86
Draickins <draickins@gmail.com>
2025-06-14 13:30:23 +0200

    base path configurable for deployment

--- src/page_generator.py
+++ src/page_generator.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise ValueError("No H1 header found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, 'r') as f:
@@ -24,6 +24,9 @@ def generate_page(from_path, template_path, dest_path):
     final_html = template_content.replace("{{ Title }}", title)
     final_html = final_html.replace("{{ Content }}", html_content)
 
+    final_html = final_html.replace('href="/', f'href="{basepath}')
+    final_html = final_html.replace('src="/', f'src="{basepath}')
+
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
@@ -31,13 +34,13 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as f:
         f.write(final_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     for entry in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, entry)
         dest_path = os.path.join(dest_dir_path, entry)
 
         if os.path.isfile(from_path) and from_path.endswith(".md"):
             dest_path_html = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, str(dest_path_html))
+            generate_page(from_path, template_path, str(dest_path_html), basepath)
         elif os.path.isdir(from_path):
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
